9b20f6dcf693b8eeeb98e3452e5e45c29d1e9c9c
raj.ramos <raj.ramos@navy.mil>
2021-10-03 08:54:58 -0700

    Add Spring Boot example

---
+++ api/src/test/java/smt/devlabs/api/DoItApplicationTests.java
@@ -0,0 +1,13 @@
+package smt.devlabs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
