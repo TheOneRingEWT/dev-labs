123e1117644f10adda26c11e84d996f3034c7636
raj.ramos <raj.ramos@navy.mil>
2021-10-14 15:28:28 -0700

    Javascript: Add 'P1 wins' when P1 chooses Paper and P2 chooses Rock Javascript: Add 'P2 wins' when P1 chooses Rock and P2 chooses Paper

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,6 +1,7 @@
 const rps = (p1, p2) => {
     if (p1 === 'Scissors' && p2 === 'Rock' ||
-        p1 === 'Paper' && p2 === 'Scissors')
+        p1 === 'Paper' && p2 === 'Scissors' ||
+        p1 === 'Rock' && p2 === 'Paper')
         return 'P2 wins'
 
     return 'P1 wins'
--- javascript/rps.spec.js
+++ javascript/rps.spec.js
@@ -57,4 +57,32 @@ describe("rps", () => {
         // Assert
         expect(actual).toBe(expected)
     })
+
+    it("should return 'P1 wins' when P1 chooses Paper and P2 chooses Rock", () => {
+        // Arrange
+        const p1 = 'Paper'
+        const p2 = 'Rock'
+
+        const expected = 'P1 wins'
+
+        // Act
+        const actual = rps(p1, p2)
+
+        // Assert
+        expect(actual).toBe(expected)
+    })
+
+    it("should return 'P2 wins' when P1 chooses Rock and P2 chooses Paper", () => {
+        // Arrange
+        const p1 = 'Rock'
+        const p2 = 'Paper'
+
+        const expected = 'P2 wins'
+
+        // Act
+        const actual = rps(p1, p2)
+
+        // Assert
+        expect(actual).toBe(expected)
+    })
 })
